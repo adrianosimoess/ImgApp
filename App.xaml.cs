4d0d4b54e57e8dfaf5ddd6761b339a85e6d2880a
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-13 20:44:08 -0400

    commit inicial

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ImgApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new MainPage(),
+                    new SegPage(),
+                    new TerPage()
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SegPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImgApp
+{
+    public partial class SegPage : ContentPage
+    {
+        public SegPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
